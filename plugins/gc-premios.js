9c55ffa5ce70ef8cbcde89aa6ce6b0f97d1c07c6
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-05-15 01:03:39 +0200

    Update gc-premios.js

--- plugins/gc-premios.js
+++ plugins/gc-premios.js
@@ -5,33 +5,30 @@ import fbDownloader from 'fb-downloader-scrapper'
 import { facebook } from "@xct007/frieren-scraper"
 import axios from 'axios'
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-if (!args[0]) throw `8 𝐕𝐄𝐑𝐒𝐔𝐒 8
+if (!args[0]) throw `PREMIOS CLAN SMACH
 
-𝐇𝐎𝐑𝐀𝐑𝐈𝐎
-🇵🇪 𝐏𝐄𝐑𝐔 : 
-🇦🇷 𝐀𝐑𝐆𝐄𝐍𝐓𝐈𝐍𝐀 : 
-🇨🇱 𝐂𝐇𝐈𝐋𝐄 :  
-𝐂𝐎𝐋𝐎𝐑 𝐃𝐄 𝐑𝐎𝐏𝐀: 
-¬ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒 𝐏𝐑𝐄𝐒𝐄𝐍𝐓𝐄𝐒
+🔰 PREMIOS ESPECIALES 🔰
 
-      𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 1
+70k DE HONOR: GANARA 10MIL 💎
+(El primero en llegar)
 
-👑 ┇ 
-🥷🏻 ┇  
-🥷🏻 ┇ 
-🥷🏻 ┇ 
-      
-     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 2
+60k DE HONOR: GANARA 3MIL 💎
+(El primero en llegar)
 
-👑 ┇ 
-🥷🏻 ┇ 
-🥷🏻 ┇ 
-🥷🏻 ┇ 
+55k DE HONOR: GANARA 1MIL 💎
+(El primero en llegar) 
 
-ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
-🥷🏻 ┇ 
-🥷🏻 ┇
-` 
+🔰 PREMIOS NORMLAES 🔰
+
+🥇TARJETA SEMANAL VALOR DE 350 💎
+🥈PASE ELITE 
+🥉PASE ELITE
+
+
+
+PERSONAS QUE QUIERAN DINERO EM EFECTIVO SE LE DEPOSITARA EL MONTÓ CORRESPONDIENTE A SU PREMIO .
+-yape  -transferencia 
+💲🇦🇷🇵🇪🇨🇱🇧🇴🇵🇾🇲🇽` 
 if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`
 try {
 await m.reply(`*[❗] ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`)      
@@ -89,10 +86,10 @@ for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url
 console.log('7 ' + err7)    
 throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*`
 }}}}}}}}}
-handler.command = /^(vs8|8vs8)$/i
-handler.register = true
+handler.command = /^(premios)$/i
+handler.register = false
 handler.group = true
-handler.admin = true
+handler.admin = false
 export default handler
 
 async function igeh(url_media) {
